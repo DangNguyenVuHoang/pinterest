96602d9e277313a164bd282a78ea8b6880871f95
DangNguyenVuHoang <dangnguyenvuhoang8384@gmail.com>
2026-02-08 11:36:11 +0700

    version 1.0 Update file sql

---
+++ pinterest_db.sql
@@ -0,0 +1,16 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : node_53
+ Source Server Type    : MySQL
+ Source Server Version : 90500 (9.5.0)
+ Source Host           : localhost:3307
+ Source Schema         : pinterest_db
+
+ Target Server Type    : MySQL
+ Target Server Version : 90500 (9.5.0)
+ File Encoding         : 65001
+
+ Date: 08/02/2026 11:33:29
+*/
+
